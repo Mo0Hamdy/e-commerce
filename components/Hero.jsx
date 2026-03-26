e4f9a61b5042e44bae146688eb4dee1b5bdace86
Mohamed Hamdy <https://github.com/Mo0Hamdy>
2026-03-26 21:45:47 +0200

    remove a paragraph from landing page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,10 +6,6 @@ export default function Hero() {
         <h2 className="text-textColor text-3xl md:text-5xl w-full md:w-150 text-center md:leading-14 pb-2">
           Sit back, relax, and let great products impress you
         </h2>
-        <p className="text-white text-xl md:text-2xl w-full md:w-160 px-2 text-center md:leading-10 pt-2">
-          Because boring is never an option. Shop smart, live stylish, and let
-          your home speak volumes.
-        </p>
         <Link href="/landing/Allproducts">
           <button className="cursor-pointer bg-black text-white p-3 rounded-xl my-4 md:my-10 font-bold hover:scale-110 duration-300">
             Go to products
